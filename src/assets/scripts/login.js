98c3ac526a0aa9fa008d3cac4cc95fff80768be2
crow <crow@crow.crow>
2021-06-21 01:42:55 +0100

    Ravengard login screen

---
+++ src/assets/scripts/login.js
@@ -0,0 +1,10 @@
+const SUBMITBUTTON = document.querySelector("#Login");
+const USERINPUT = document.querySelector("#UserInput");
+const PASSWORDINPUT = document.querySelector("#PasswordInput");
+
+
+SUBMITBUTTON.preventDefault();
+
+SUBMITBUTTON.addEventListener("click", () => {
+    console.log("hello world!");
+})
