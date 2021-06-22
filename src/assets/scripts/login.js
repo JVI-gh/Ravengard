6b93e68c9cad481329f074973e695775562395c8
crow <crow@crow.crow>
2021-06-22 02:22:02 +0100

    Adding second screen which is gonna be the main app

--- src/assets/scripts/login.js
+++ src/assets/scripts/login.js
@@ -1,10 +1,42 @@
 const SUBMITBUTTON = document.querySelector("#Login");
 const USERINPUT = document.querySelector("#UserInput");
 const PASSWORDINPUT = document.querySelector("#PasswordInput");
+const APIserver = "https://IGAPI.callmecrow.repl.co/";
+
+SUBMITBUTTON.addEventListener("click", (e) => {
+    e.preventDefault();
+    let username = USERINPUT.value;
+    let password = PASSWORDINPUT.value;
+
+    login(username, password);
+});
+
+login = (username, password) => {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+      username: username,
+      password: password,
+    });
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
 
 
-SUBMITBUTTON.preventDefault();
-
-SUBMITBUTTON.addEventListener("click", () => {
-    console.log("hello world!");
-})
+    fetch(APIserver + "login", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.token) {
+            window.location.href = "./assets/screens/main.html";
+          try {
+          } catch (e) {
+            // saving error
+          }
+        }
+      }).catch((error) => console.log("error", error));
+  };
